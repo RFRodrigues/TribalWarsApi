31d70eedd9ed94ad8aa8cd5454c01e5acfaa5c3f
Rúben Filipe Santiago Lomba  Rodrigues <rrodrigues@smartconsulting.pt>
2020-06-11 14:18:55 +0100

    api added

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const firebase = require("firebase");
 
 const port = 5000;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbGn-CM2XXJAmhkTERlsGDRyYryij9D4g",
+  authDomain: "tribalwars-15493.firebaseapp.com",
+  databaseURL: "https://tribalwars-15493.firebaseio.com",
+  projectId: "tribalwars-15493",
+  storageBucket: "tribalwars-15493.appspot.com",
+  messagingSenderId: "1009323283348",
+  appId: "1:1009323283348:web:465fb1bed784908d33f581",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +30,14 @@ app.get("/users", (req, res) => {
     { name: "William", location: "Abu Dhabi" },
     { name: "Chris", location: "Vegas" }
   ]);
+});
+
+app.get("/usersinfo", (req, res) => {
+  var db = firebase.database().ref("/users");
+  db.once("value", (snapshot) => {
+    var users = snapshot.val();
+    res.send(users);
+  });
 });
 
 app.post("/user", (req, res) => {
